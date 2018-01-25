29c67c6ef96bc90c9e5ac9528c3309384dfd2edb
Miodrag Vujković <miodrag.vujkovic@alter.org>
2018-01-25 13:23:15 +0100

    some bug fixes

--- server.go
+++ server.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/labstack/echo"
-	"net/http"
-	"github.com/labstack/echo/middleware"
+	// "github.com/labstack/echo/middleware"
 	"github.com/gorilla/websocket"
+	"github.com/labstack/echo/middleware"
+	"encoding/json"
 )
 
 
@@ -18,47 +19,74 @@ type Message struct {
 var (
 	upgrader = websocket.Upgrader{}
     clients = make(map[string]*websocket.Conn)
+    broadcast = make(chan Message)
 )
 
-func hello(c echo.Context) error {
+
+func handleConnections(c echo.Context) error {
 	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
-	if err != nil {
-		return c.String(http.StatusInternalServerError, "greska")
+    if err != nil {
+    	fmt.Println("Nisam mogao da upalim websockete")
 	}
 	defer ws.Close()
 
-    for {
+	for {
 		// Read
 		msg := Message{}
-		err = ws.ReadJSON(&msg)
+		_, message, err := ws.ReadMessage()
 		if err != nil {
-			c.Logger().Error(err)
+			if websocket.IsUnexpectedCloseError(err, 0) {
+				fmt.Println("Ugasi se korisnik: ")
+				delete(clients, "")
+			}
+			break
+		}
+		fmt.Println("poruka: ", message)
+		fmt.Println("greska: ", err)
+		err = json.Unmarshal(message, &msg)
+		if err != nil {
 			fmt.Println("ne mogu da procitam JSON")
 		}
+
 		fmt.Printf("%s\n", msg)
 
 		clients[msg.Username] = ws
+
+
+		broadcast <- msg
+
+	}
+	return nil
+}
+
+func handleMessages() {
+	for {
+		msg := <- broadcast
 		sender := msg.Username
 		receiver := msg.Receiver
 		text := sender + ": " + msg.Message
+
 		if receiverWs, ok := clients[receiver]; ok {
 			// Write
 			err := receiverWs.WriteMessage(websocket.TextMessage, []byte(text))
 			if err != nil {
-				c.Logger().Error(err)
 				fmt.Println("greska u slanju poruke")
 			}//do something here
 		}
-
 	}
 
 }
 
 func main() {
+
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.File("/", "public/index.html")
-	e.GET("/ws", hello)
+	e.GET("/ws", handleConnections)
+
+	go handleMessages()
+
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
